3961bc0c1ff898469c7637a367f927b6894049d9
hello-devs <30301241+hello-devs@users.noreply.github.com>
2018-02-20 17:46:22 +0100

    nettoyage lisibilité

--- js/reservation.js
+++ js/reservation.js
@@ -1,35 +1,48 @@
 var reservation = {};
 
+//On vérifie la présence d'une réservation à la connection
 checkStatutReservation();
 
+//
+function initReservation() {
+    reservation = {
+        stationName: map.stationSelect.name,
+        address: map.stationSelect.address,
+        client: $('#name').val() + " " + $('#firstname').val(),
+        dateReservation: new Date(),
+        sign: this.sign,
+        /*limit: null,
+        dlcMin: null,
+        dlcSec: null,*/
+    };
+}
 
 
 function checkStatutReservation() {
     if (sessionStorage.getItem('reservation')) {
-
         showFooterReservation();
-
     } else {
-
         console.log('Aucune reservation en cours');
     }
-
 }
 
+//Stocage en sessionStorage
+function reservationStorage() {
+    var reservation_json = JSON.stringify(reservation);
+    //Stoque la reservation  dans la sessionstorage
+    sessionStorage.setItem('reservation', reservation_json);
+}
 
-function initReservation() {
-
-    //Remplace les info station par le formulaire de reservation
-
+function showReservationForm() {
+    //Remplace les infos station par le formulaire de reservation
     $('#infoStation').css('display', 'none');
     $('#infoReservation').css('display', 'block');
     $('#name').focus();
 
-    //Indique que le formule n'a pas été signé de base
+    //Indique que le formulaire n'a pas été signé de base
     reservation.sign = false;
-    console.log(reservation); //test
 
-        // Bouton Reset :
+    // Bouton Reset :
     $("#reset").click(function () {
         reservation.sign = false;
     });
@@ -39,22 +52,13 @@ function initReservation() {
         console.log(reservation); //test
         if (!reservation.sign) {
             /////////////Modal////////////////////
-        $('#signReservation').modal();
+            $('#signReservation').modal();
         } else {
             //Recuperation en variable global
-            reservation = {
-                stationName: map.stationSelect.name,
-                address: map.stationSelect.address,
-                client: this.name.value + " " + this.firstname.value,
-                dateReservation: new Date(),
-                sign: reservation.sign,
-                limit: null,
-                dlcMin: null,
-                dlcSec: null,
-            };
-            var reservation_json = JSON.stringify(reservation);
-            //Stoque la reservation  dans la sessionstorage
-            sessionStorage.setItem('reservation', reservation_json);
+            initReservation();
+
+            reservationStorage();
+
             $('#infoReservation').css('display', 'none');
             $('#map').removeClass('col s12 l8').addClass('col s12');
 
@@ -93,10 +97,11 @@ function showFooterReservation() {
         expire();
         /////////////Modal////////////////////
         $('#annulReservation').modal();
-        //////////////////////////////////////////////Solution temporaire:
+        ////////////////////////////////////////Solution temporaire:
         setTimeout(function () {
             window.location = ("./index.html");
-        }, 3000);
+        }, 2000);
+
 
     });
 
@@ -104,33 +109,24 @@ function showFooterReservation() {
 
 function delaiReservation() {
     var dateReservation = new Date(reservation.dateReservation);
-    //console.log("dateReservation: " + dateReservation);
-
     reservation.limit = new Date(dateReservation.setMinutes(dateReservation.getMinutes() + 20)); //1 Pour test
-    //console.log("reservation.limit :" + reservation.limit);
-    //console.log("now: " + Date());
-
     var now = new Date();
 
     if (now <= reservation.limit) {
 
         if (now.getTime() < reservation.limit.getTime()) {
             console.log("Réservation valide");
-
             //Actualiser ttes les sec
             reservation.actualisationTps = setInterval(calcMinSec, 1000);
-            //console.log(reservation); //test
-
         } else {
             console.log("expiré");
-            //clearInterval(reservation.actualisationTps);
             expire();
             /////////////Modal////////////////////
             $('#expiReservation').modal();
             //////////////////////////////////////////////Solution temporaire:
             setTimeout(function () {
                 window.location = ("./index.html");
-            }, 3000);
+            }, 2000);
         }
 
     } else {
@@ -142,7 +138,7 @@ function delaiReservation() {
         //////////////////////////////////////////////Solution temporaire:
         setTimeout(function () {
             window.location = ("./index.html");
-        }, 3000);
+        }, 2000);
     }
 
 
@@ -155,52 +151,40 @@ function calcMinSec() {
 
     if (now.getTime() < reservation.limit.getTime()) {
         console.log("Réservation valide");
-        //Actualiser ttes les sec
         var dlc = (reservation.limit - new Date());
-        //console.log(dlc);
         //nombre de minute(s) restante(s)
         reservation.dlcMin = parseInt(dlc / (60 * 1000));
-        //console.log(reservation.dlcMin);
         //nombre de seconde(s) restante(s)
         reservation.dlcSec = parseInt(dlc % (60 * 1000) / 1000);
-        //console.log(reservation.dlcSec);
 
         $('#footerDlcMin').text(reservation.dlcMin);
         $('#footerDlcSec').text(reservation.dlcSec);
 
     } else {
         console.log("expiré");
-        //clearInterval(reservation.actualisationTps);
         expire();
         /////////////Modal////////////////////
         $('#expiReservation').modal();
         //////////////////////////////////////////////Solution temporaire:
         setTimeout(function () {
             window.location = ("./index.html");
-        }, 3000);
+        }, 2000);
 
     }
 
 }
 
 
-
+//supprimer la reservation
 function expire() {
 
     clearInterval(reservation.actualisationTps);
-
-    //reservation.actualisationTps = null;
-    reservation.dlcMin = null;
-    reservation.dlcSec = null;
     reservation = {};
+
 
     sessionStorage.clear();
     console.log(reservation);
     //Changement Info footer
     $('#reservationFooter').css('display', 'none');
     $('#noReservationFooter').css('display', 'block');
-
-
-
-
 }
