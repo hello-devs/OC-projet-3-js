0fc02f11c8521dacba2954864cc072c0a9d455be
hello-devs <30301241+hello-devs@users.noreply.github.com>
2018-08-12 10:15:59 +0200

    integration class Mapp et Slider

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var sliderElt = new Slider("divHelp", "prevArrow", "nextArrow");
+var mapElt = new Mapp;
+
+/*
+var reservation = new Reservation;
+//On vérifie la présence d'une réservation à la connection
+reservation.checkStatutReservation();
+*/
