3435ecbf5f67c60ce2c47d6648c85bd8ab76ab6f
hello-devs <30301241+hello-devs@users.noreply.github.com>
2018-08-19 14:30:05 +0200

    maj poo

--- js/SSlider.js
+++ js/SSlider.js
@@ -1,9 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**/
 class Slider {
 
 
@@ -61,4 +55,3 @@ class Slider {
     }
 
 }
-
